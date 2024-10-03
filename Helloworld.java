69752091cff16df0c1ffe739a107a2637d8c3d06
Harikrishna-hari <22wj1a05x4gniindia.org>
2024-10-03 12:42:31 +0530

    comming the files

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,7 @@
-class Helloworld 
+class Helloworld
 {
-public static void main(string args[])
+public static void main(String args[])
 {
-System.out.println("Hello world");
+System.out.println("hello world");
 }
 }
